5dd093c11d2a60a01c161126df0dad92e1206887
Dan <daniel.longworth@colony101.co.uk>
2013-07-03 12:56:31 +0100

    Simple Mime Message support.

---
+++ MantaCore/Message/MimeMessageBodyPart.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Mime;
+using System.Text;
+
+namespace MantaMTA.Core.Message
+{
+	public class MimeMessageBodyPart
+	{
+		/// <summary>
+		/// Holds the boundry
+		/// </summary>
+		public string Boundry { get; set; }
+		/// <summary>
+		/// The content type of the body part.
+		/// </summary>
+		public ContentType ContentType { get; set; }
+		/// <summary>
+		/// The Transfer encoding for the body part.
+		/// </summary>
+		public TransferEncoding TransferEncoding { get; set; }
+		/// <summary>
+		/// The Transfer encoded body
+		/// </summary>
+		public string EncodedBody { get; set; }
+
+		/// <summary>
+		/// If true then this body part contains a MIME Message
+		/// </summary>
+		public bool HasChildMimeMessage
+		{
+			get
+			{
+				if (ContentType == null)
+					return false;
+
+				return ContentType.MediaType.Equals("message/rfc822", StringComparison.OrdinalIgnoreCase);
+			}
+		}
+
+		/// <summary>
+		/// Child message or null
+		/// </summary>
+		public MimeMessage ChildMimeMessage
+		{
+			get
+			{
+				if (!HasChildMimeMessage)
+					return null;
+
+				return MimeMessage.Parse(this.GetDecodedBody());
+			}
+		}
+
+		public MimeMessageBodyPart()
+		{
+			TransferEncoding = TransferEncoding.Unknown;
+		}
+
+		/// <summary>
+		/// Get this Body Part decoded.
+		/// </summary>
+		/// <returns>Body part body with Transport Encoding decoded.</returns>
+		public string GetDecodedBody()
+		{
+			string tmp = EncodedBody.Trim();
+			if (TransferEncoding == TransferEncoding.Base64)
+			{
+				tmp = UTF8Encoding.Default.GetString(Convert.FromBase64String(tmp));
+			}
+			return tmp;
+		}
+	}
+}
---
+++ MantaCoreTests/MimeMessageTests.cs
@@ -0,0 +1,118 @@
+﻿using MantaMTA.Core.Message;
+using NUnit.Framework;
+
+namespace MantaMTA.Core.Tests
+{
+	[TestFixture]
+	class MimeMessageTests
+	{
+		/// <summary>
+		/// Test what happens when a mime message is passed something it can't handle.
+		/// Should return null.
+		/// </summary>
+		[Test]
+		public void NotAMimeMessage()
+		{
+			string testMessage = "Not a mime message";
+			MimeMessage msg = MimeMessage.Parse(testMessage);
+			Assert.IsNull(msg);
+		}
+
+		/// <summary>
+		/// Test attempting to parse a mime message with no content-type header.
+		/// Should return null.
+		/// </summary>
+		[Test]
+		public void NoContentTypeHeader()
+		{
+			string testMessage = @"from: <test@remote>
+to: <test@localhost>
+
+Hi";
+			MimeMessage msg = MimeMessage.Parse(testMessage);
+			Assert.IsNull(msg);
+		}
+
+		/// <summary>
+		/// Test a non multipart message.
+		/// Should be null.
+		/// </summary>
+		[Test]
+		public void NotMultipart()
+		{
+			string testMessage = @"from: <test@remote>
+to: <test@localhost>
+content-type: text/plain
+
+Hi";
+			MimeMessage msg = MimeMessage.Parse(testMessage);
+			Assert.IsNull(msg);
+		}
+
+		/// <summary>
+		/// Test a mime message with no child messages in any body parts.
+		/// </summary>
+		[Test]
+		public void NoChildMessages()
+		{
+			string testMessage = @"from: <test@remote>
+to: <test@localhost>
+content-type: multipart/mime
+
+--boundry_one
+
+Body Part One!
+
+--boundry_one
+
+Body Part Two!
+
+--boundry_one--";
+			MimeMessage msg = MimeMessage.Parse(testMessage);
+			Assert.NotNull(msg);
+			Assert.AreEqual(2, msg.BodyParts.Length);
+		}
+
+		/// <summary>
+		/// Test a mime message with a body part that contains a message.
+		/// </summary>
+		[Test]
+		public void ChildMessage()
+		{
+			string testMessage = @"from: <test@remote>
+to: <test@localhost>
+content-type: multipart/mime
+
+--boundry_one
+
+Body Part One!
+
+--boundry_one Content-Type: message/rfc822
+
+To: <test@remote>
+From: <test@local>
+Content-Type: multipart/mime
+
+--boundry_two 
+
+Body Part Two.One!
+
+--boundry_two
+
+Body Part Two.Two!
+
+--boundy_two--
+
+--boundry_one--";
+			MimeMessage msg = MimeMessage.Parse(testMessage);
+			Assert.NotNull(msg);
+			Assert.AreEqual(2, msg.BodyParts.Length);
+
+			Assert.IsFalse(msg.BodyParts[0].HasChildMimeMessage);
+			Assert.IsTrue(msg.BodyParts[1].HasChildMimeMessage);
+			
+			Assert.NotNull(msg.BodyParts[1].ChildMimeMessage);
+			Assert.NotNull(msg.BodyParts[1].ChildMimeMessage.BodyParts);
+		}
+	}
+}
