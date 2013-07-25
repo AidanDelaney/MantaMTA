f763e68442254de08228b5d047181d58935e24b1
Dan <daniel.longworth@colony101.co.uk>
2013-07-25 16:44:03 +0100

    Event Type/Bounce Code/Bounce Type in DB

---
+++ Database/MtaDB/MANTA_MTA/Tables/man_evn_bounceCode.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[man_evn_bounceCode](
+	[evn_bounceCode_id] [int] NOT NULL,
+	[evn_bounceCode_name] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[evn_bounceCode_description] [nvarchar](250) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+
+GO
+
---
+++ Database/MtaDB/MANTA_MTA/Tables/man_evn_type.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[man_evn_type](
+	[evn_type_id] [int] NOT NULL,
+	[evn_type_name] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[evn_type_description] [nvarchar](250) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+
+GO
+
