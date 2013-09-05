84ed8ed5867ce09d949e55eec88ac7f90f80be1a
Dan <daniel.longworth@colony101.co.uk>
2013-09-05 17:16:45 +0100

    Added settings tab, read only

---
+++ WebInterface/Controllers/SettingsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MantaMTA.Core.DAL;
+using WebInterface.Models;
+
+namespace WebInterface.Controllers
+{
+    public class SettingsController : Controller
+    {
+        //
+        // GET: /Settings/
+        public ActionResult Index()
+        {
+			return View(new SettingsModel
+			{
+				ClientIdleTimeout = CfgPara.GetClientIdleTimeout(),
+				DaysToKeepSmtpLogsFor = CfgPara.GetDaysToKeepSmtpLogsFor(),
+				DefaultVirtualMtaGroup = VirtualMtaGroupDB.GetVirtualMtaGroup(CfgPara.GetDefaultIPGroupID()),			
+				EventForwardingUrl = CfgPara.GetEventForwardingHttpPostUrl(),
+				LocalDomains = CfgLocalDomains.GetLocalDomainsArray(),
+				MaxTimeInQueue = CfgPara.GetMaxTimeInQueueMinutes(),
+				ReceiveTimeout = CfgPara.GetReceiveTimeout(),
+				RelayingPermittedIPs = CfgRelayingPermittedIP.GetRelayingPermittedIPAddresses(),
+				RetryInterval = CfgPara.GetRetryIntervalMinutes(),
+				ReturnPathDomain = CfgPara.GetReturnPathDomain(),
+				SendTimeout = CfgPara.GetSendTimeout()
+			});
+        }
+
+    }
+}
---
+++ WebInterface/Models/SettingsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MantaMTA.Core.VirtualMta;
+
+namespace WebInterface.Models
+{
+	/// <summary>
+	/// Model for the Settings page.
+	/// </summary>
+	public class SettingsModel
+	{
+		public int ClientIdleTimeout { get; set; }
+		public int DaysToKeepSmtpLogsFor { get; set; }
+		public VirtualMtaGroup DefaultVirtualMtaGroup { get; set; }
+		public string EventForwardingUrl { get; set; }
+		public string[] LocalDomains { get; set; }
+		public int MaxTimeInQueue { get; set; }
+		public string[] RelayingPermittedIPs { get; set; }
+		public int ReceiveTimeout { get; set; }
+		public int RetryInterval { get; set; }
+		public string ReturnPathDomain { get; set; }
+		public int SendTimeout { get; set; }
+	}
+}
