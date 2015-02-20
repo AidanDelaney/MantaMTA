76f97bd6203920db955b3ae7527460f730ed39da
Dan <daniel.longworth@colony101.co.uk>
2015-02-20 14:00:40 +0000

    Send report pages show send metadata

--- WebInterface/Models/Sends.cs
+++ WebInterface/Models/Sends.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MantaMTA.Core.Sends;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,6 +47,27 @@ namespace WebInterface.Models
 		public SendReportBase(string sendID)
 		{
 			this.SendID = sendID;
+		}
+
+		/// <summary>
+		/// Gets a nice string of the metadata to display on Send Report pages.
+		/// </summary>
+		/// <returns></returns>
+		public string GetSendReportPageTitle()
+		{
+			int internalID = SendManager.Instance.GetSend(this.SendID).InternalID;
+
+			SendMetadataCollection meta = new SendMetadataCollection(WebInterfaceLib.DAL.SendDB.GetSendMetaData(internalID).OrderBy(m => m.Name));
+			
+			string result = string.Empty;
+			for (int i = 0; i < meta.Count; i++)
+			{
+				if (i > 0)
+					result += ", ";
+				result += meta[i].Value;
+			}
+
+			return result;
 		}
 	}
 
