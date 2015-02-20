c15ca53c07b2714f602090334269daab78b1970a
Dan <daniel.longworth@colony101.co.uk>
2015-02-20 17:15:27 +0000

    Added basic RabbitMQ queue page

---
+++ WebInterface/Models/RabbitMqModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace WebInterface.Models
+{
+	/// <summary>
+	/// Holds information about a RabbitMQ queue.
+	/// </summary>
+	public class RabbitMqQueue
+	{
+		/// <summary>
+		/// Name of the Queue.
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Messages in the queue.
+		/// </summary>
+		public long Messages { get; set;}
+		/// <summary>
+		/// State of the queue.
+		/// </summary>
+		public string State { get; set; }
+	}
+
+	public class RabbitMqQueueModel : List<RabbitMqQueue>
+    {
+        
+    }
+}
