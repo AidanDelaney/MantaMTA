34d3e365bb0a65b5e300d0a882b1fbdaedaa4d2a
Dan <daniel.longworth@colony101.co.uk>
2015-08-21 13:46:12 +0100

    Removed assets, added with Nuget

---
+++ WebInterface/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Optimization;
+
+namespace WebInterface
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			BundleTable.EnableOptimizations = false;
+
+			// Scripts
+			bundles.Add(new ScriptBundle("~/resources/js/jquery").Include("~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/resources/js/bootstrap").Include("~/Scripts/bootstrap.js"));
+			bundles.Add(new ScriptBundle("~/resources/js/jqplot").Include("~/Scripts/jqplot/jquery.jqplot.min.js"));
+
+
+			// Styles
+			bundles.Add(new StyleBundle("~/resources/css/bootstrap").Include("~/Content/bootstrap.css",
+																			 "~/Content/bootstrap-responsive.css",
+																			 "~/Content/app.css",
+																			 "~/Content/fonts.css"));
+			bundles.Add(new ScriptBundle("~/resources/css/jqplot").Include("~/Scripts/jqplot/jquery.jqplot.min.css"));
+		}
+	}
+}
